30351eaa9a7e667aa9c0a59c99c1490e904726f6
Marco Arthur <arthurpbs@gmail.com>
2017-12-12 08:52:49 -0200

    Adding docs to Gorjun
    Documentation for Gorjun users.

--- lib/Gorjun.pm
+++ lib/Gorjun.pm
@@ -458,3 +458,97 @@ sub clearsign_msg {
 }
 
 1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Gorjun - Perl Client to L<gorjun daemon|https://github.com/subutai-io/gorjun>
+
+=head1 VERSION
+
+Version 0.1
+
+=head1 SYNOPSIS
+
+ my $g = Gorjun->new(
+    host => 'http://127.0.0.1',
+    port => '8080',
+    user => 'Tester_User',
+    gpg_pass_phrase => 'secret', # needed passphrase for gpg signing
+ );
+
+ # Get token
+ $g->get_token( message => 'Some message', user => 'Tester_User' );
+
+=head1 DESCRIPTION
+
+This modules operates gorjun daemon using its rest interface
+so you can control gorjun actions in a Perl environment, sending, receiving
+messages. Useful for integration tests as well as system administration of
+gorjun. There are high level operations, as well as low level.
+
+=head2 High Level
+
+Those are methods that performs a grouped set of low level operations. Such as
+register a new user, upload a file, or sign an uploaded file. All of these won't
+be executed with only one REST call, and will need to send several queries in
+order to complete. All high level operations and their arguments are listed
+below:
+
+=over 4
+
+=item *
+
+register( name => $n, key => $k )
+
+=item *
+
+get_authid( user => $u )
+
+=item *
+
+get_token( user => $u, message => $m )
+
+=item *
+
+quota( user => $u, token => $t )
+
+=item *
+
+sign( signature => $s, token => $t )
+
+=item *
+
+upload( file => $f, token => $t , type => $t)
+
+=back
+
+=head2 Low Level
+
+Those are simple one REST call, generally only completing a step of the High Level
+operation. They map to the REST API exactly one-to-one:
+L<https://github.com/subutai-io/gorjun/wiki/v1>
+
+And all of them are completed with C<send()> interface. Example:
+
+ # list all raw files
+ $g->send( 
+        method => 'get'
+        path   => '/kurjun/rest/raw/download'
+ );
+
+=head2 BUGS AND KNOWN ISSUES
+
+Gorjun depends on gpg utility, since messages should be signed using a GPG key
+L<https://www.gnupg.org/> . Not all operations can be completed without user intervention,
+since not all gpg functions can be used in a scripted manner, requiring user to manually
+type the pass phrase at least once.
+
+=head2 LICENSE
+
+This software is GNU GPL3 licensed, and can be freely copied, modified and redistributed.
